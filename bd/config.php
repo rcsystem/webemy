27efdd6cb83bcdc1b730a130f1f3885babb3ccb2
vicente <ing-vicente-dr@hotmail.com>
2018-03-02 10:12:49 -0600

    Se programo el registro

--- bd/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    define('DB_HOST','localhost');
-	define('DB_USER','administrador');
-	define('DB_PASS','rafasistem');
-	define('DB_NAME','desarrollo_emy');
-	define('DB_CHARSET','utf8');
- ?>
--- registro.php
+++ registro.php
@@ -3,10 +3,59 @@
 //Creado por:
 // rafael cruz aguilar rafael_cruz_aguilar@hotmail.com
 // ============================================================================= -->
+<?php
+session_start();
+if (isset($_SESSION['userSession'])!="") {
+    header("Location:.php");
+}
+require_once 'ConexionBD/dbconnect.php';
+$msg =""; 
+if(isset($_POST['btn-registro'])) {
+$nombre = $_POST["nombre_edecan"];
+$apellido = $_POST["ape_edecan"];
+$correo = $_POST["email_edecan"];
+$password = $_POST["pass_edecan"];
+$telefono = $_POST["tel_edecan"];
+$celular = $_POST["movil_edecan"];
+$direccion = $_POST["dir_edecan"];
+$genero = $_POST["genero_edecan"];
+$contrasena_incriptada = password_hash($password, PASSWORD_DEFAULT);
+
+
+
+
+    $sql1 = $DBcon->query("SELECT ede_email FROM emy_edecanes WHERE ede_email='$correo'");
+    $count=$sql1->num_rows;
+    
+    if ($count==0) {
+        
+$query = "INSERT INTO emy_edecanes(ede_nombre, ede_apellido, ede_email, ede_password, ede_tel, ede_movil, ede_dir, ede_genero) VALUES('$nombre', '$apellido', '$correo', '$contrasena_incriptad','$telefono','$celular','$direccion','$genero')";
+
+
+        if ($DBcon->query($query)) {
+            $msg = "<div class='alert alert-success'>
+                        <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Registro Exitoso!
+                    </div>";
+        }else {
+            $msg = "<div class='alert alert-danger'>
+                        <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Error fatal 'No se puedo registrar'!
+                    </div>";
+        }
+        
+    } else {
+        
+        
+        $msg = "<div class='alert alert-danger'>
+                    <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Lo lamento correo ya en uso!
+                </div>";
+            
+    }
+    
+    $DBcon->close();
+}
+?>
+
 <?php require_once"header.php"; ?>
-
-<div class="home_juntar">
-
 <section>
 
 <div class="content-reg">
@@ -22,7 +71,7 @@
 
 
                 <div class="col-lg-6">
-                <form action="index.php/registro/registro_edecan" method="POST">
+                <form action="registro.php" method="POST" utf-8>
                 <div class="col-lg-12">
                 <div class="form-group">
                 <label for="tipo_cuenta">Selecciona tu tipo de cuenta</label>
@@ -108,7 +157,8 @@
                   
 
                      <div class="btn-registro">
-                         <input type="submit" class="btn btn-block btn-default btn-reg" value="Crear una cuenta">
+                         <input type="submit" class="btn btn-block btn-default btn-reg" value="Crear una cuenta" name="btn-registro">
+                         <?php echo $msg; ?>
                      </div>
                     
                     </form>
@@ -121,5 +171,5 @@
 </div>
 
 </section>
-</div>
+
 <?php require_once"footer.php"; ?>
